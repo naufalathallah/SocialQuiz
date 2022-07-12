6df97100fe990cf3236113857e2354e6ac5e2a57
Naufal Athallah Iwel <naufalaiwel@gmail.com>
2022-07-12 21:50:09 +0700

    initial commit

---
+++ src/socialquiz/SocialQuiz.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socialquiz;
+
+/**
+ * Copyright naufal athallah iwel & raihan ramadhani 2020
+ * @author NAUFAL
+ */
+public class SocialQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    private int nilai = 0;
+    public int getInt(){
+        return nilai;
+    }
+    
+    public void setInt(Integer nilai){
+        this.nilai = 0;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
